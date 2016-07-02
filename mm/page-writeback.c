316c2fa4741391321cd14b9eb56c9d5ce6e66a49
Joe Maples <joe@frap129.org>
2016-07-02 17:34:56 +0000

    zen-tune: implement zen-tune v4.5
    Based on https://github.com/zen-kernel/zen-kernel/commit/8e64dff0ddc573d4d0832cb572fcaabc0ff48ec2 but slightly modified for android.
    
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    # Conflicts:
    #	arch/arm64/configs/benzo_defconfig
    #	init/Kconfig
    #	kernel/sched/fair.c
    #	mm/page-writeback.c

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -71,7 +71,7 @@ static long ratelimit_pages = 32;
  * Start background writeback (via writeback threads) at this percentage
  */
 #ifdef CONFIG_ZEN_INTERACTIVE
-int dirty_background_ratio;
+int dirty_background_ratio = 20;
 #else
 int dirty_background_ratio = 10;
 #endif
@@ -96,7 +96,7 @@ int vm_highmem_is_dirtyable;
  * The generator of dirty data starts writeback at this percentage
  */
 #ifdef CONFIG_ZEN_INTERACTIVE
-int vm_dirty_ratio;
+int vm_dirty_ratio = 30;
 #else
 int vm_dirty_ratio = 20;
 #endif
