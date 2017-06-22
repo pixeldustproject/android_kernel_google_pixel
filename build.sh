84de418bde5a3ede30b23f173f6b9dff628934d2
Dustin Rinne <dustin.rinne@gmail.com>
2017-06-21 22:11:28 -0500

    Give pissel a name!
    Change-Id: Ica10f6f17f945da6dd95e51d257f170d35eb7e88

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ export CROSS_COMPILE=${HOME}/tipsy/prebuilts/gcc/linux-x86/aarch64/aarch64-8.0/b
 DEFCONFIG="fuckery_defconfig"
 
 # Kernel Details
-VER=".get-fucked"
+VER=".pissel"
 
 # Paths
 KERNEL_DIR=`pwd`
